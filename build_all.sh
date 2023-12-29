6f7a57e4aace49cd8de0349a44db5a8cb061acde
donfarmwise <152221416+donfarmwise@users.noreply.github.com>
2023-12-29 09:51:40 -0800

    adding VVDN.patch
    Linux_for_Tegra/bootloader/t186ref/BCT/tegra234-mb2-bct-misc-p3767-0000.dts
    Linux_for_Tegra/p3767.conf.common
    Linux_for_Tegra/source/public/hardware/nvidia/platform/t23x/p3768/kernel-dts/cvb/tegra234-camera-rbpcv4-imx296.dtsi            ->missing
    Linux_for_Tegra/source/public/hardware/nvidia/platform/t23x/p3768/kernel-dts/cvb/tegra234-p3509-a02-pcie.dtsi
    Linux_for_Tegra/source/public/hardware/nvidia/platform/t23x/p3768/kernel-dts/cvb/tegra234-p3509-a02.dtsi                        
    Linux_for_Tegra/source/public/hardware/nvidia/platform/t23x/p3768/kernel-dts/cvb/tegra234-p3768-0000-a0-pcie.dtsi               ->****here
    Linux_for_Tegra/source/public/hardware/nvidia/platform/t23x/p3768/kernel-dts/cvb/tegra234-p3768-0000-a0.dtsi
    Linux_for_Tegra/source/public/hardware/nvidia/platform/t23x/p3768/kernel-dts/cvb/tegra234-p3768-camera-rbpcv2-imx219.dtsi
    Linux_for_Tegra/source/public/hardware/nvidia/platform/t23x/p3768/kernel-dts/cvb/tegra234-p3768-camera-rbpcv4-imx296.dtsi      ->missing
    Linux_for_Tegra/source/public/kernel/kernel-5.10/arch/arm64/configs/defconfig
    Linux_for_Tegra/source/public/kernel/nvidia/drivers/media/i2c/Kconfig
    Linux_for_Tegra/source/public/kernel/nvidia/drivers/media/i2c/Makefile
    Linux_for_Tegra/source/public/kernel/nvidia/drivers/media/i2c/imx296_mode_tbls.h               ->missing
    Linux_for_Tegra/source/public/kernel/nvidia/drivers/media/i2c/nv_imx296.c                      ->missing
    Linux_for_Tegra/source/public/kernel/nvidia/drivers/media/platform/tegra/camera/camera_common.c
    Linux_for_Tegra/source/public/kernel/nvidia/drivers/media/platform/tegra/camera/sensor_common.c
    Linux_for_Tegra/source/public/kernel/nvidia/drivers/media/platform/tegra/camera/vi/vi5_formats.h
    flashScri.sh
    kernel_compile
    
        iv)go to 
    
    -------------------------------------------------
    diff -ur Linux_for_Tegra/bootloader/t186ref/BCT/tegra234-mb2-bct-misc-p3767-0000.dts ~/Downloads/VVDN_DON2/vvdn-linux/Linux_for_Tegra/bootloader/t186ref/BCT/tegra234-mb2-bct-misc-p3767-0000.dts > VVDN.patch
    
    diff -ur Linux_for_Tegra/p3767.conf.common ~/Downloads/VVDN_DON2/vvdn-linux/Linux_for_Tegra/p3767.conf.common >> VVDN.patch
    
    diff -ur Linux_for_Tegra/source/public/hardware/nvidia/platform/t23x/p3768/kernel-dts/cvb/tegra234-camera-rbpcv4-imx296.dtsi ~/Downloads/VVDN_DON2/vvdn-linux/Linux_for_Tegra/source/public/hardware/nvidia/platform/t23x/p3768/kernel-dts/cvb/tegra234-camera-rbpcv4-imx296.dtsi  >> VVDN.patch
    
    
    diff -ur Linux_for_Tegra/source/public/hardware/nvidia/platform/t23x/p3768/kernel-dts/cvb/tegra234-p3509-a02-pcie.dtsi ~/Downloads/VVDN_DON2/vvdn-linux/Linux_for_Tegra/source/public/hardware/nvidia/platform/t23x/p3768/kernel-dts/cvb/tegra234-p3509-a02-pcie.dtsi >> VVDN.patch
    
    
    diff -ur Linux_for_Tegra/source/public/hardware/nvidia/platform/t23x/p3768/kernel-dts/cvb/tegra234-p3509-a02.dtsi ~/Downloads/VVDN_DON2/vvdn-linux/Linux_for_Tegra/source/public/hardware/nvidia/platform/t23x/p3768/kernel-dts/cvb/tegra234-p3509-a02.dtsi >> VVDN.patch
    
    
    
    diff -ur Linux_for_Tegra/source/public/hardware/nvidia/platform/t23x/p3768/kernel-dts/cvb/tegra234-p3768-0000-a0-pcie.dtsi ~/Downloads/VVDN_DON2/vvdn-linux/Linux_for_Tegra/source/public/hardware/nvidia/platform/t23x/p3768/kernel-dts/cvb/tegra234-p3768-0000-a0-pcie.dtsi >> VVDN.patch

--- build_all.sh
+++ build_all.sh
@@ -38,19 +38,23 @@ cd $ROOT
 
 sudo tar -xvf public_sources.tbz2
 
+#sudo tar -xjf public_sources.tbz2 -C Linux_for_Tegra/source/public --strip-components=3
+
+# sudo tar -xjf public_sources.tbz2 -C Linux_for_Tegra/source/public
+
+
+# sudo tar -xjf public_sources.tbz2
+
+
+
 cd $ROOT
 
 mkdir aarch64--glibc--stable-final
 
 sudo tar -xvf aarch64--glibc--stable-final.tar.gz -C aarch64--glibc--stable-final
 
-cd $ROOT
 
 
-cd Linux_for_Tegra/
-
-sudo ./apply_binaries.sh
-sudo ./tools/l4t_flash_prerequisites.sh
 
 cd $ROOT
 
@@ -63,45 +67,24 @@ sudo tar -xvf kernel_src.tbz2
 cd $ROOT
 
 
+cd Linux_for_Tegra/
+
+sudo ./apply_binaries.sh
+sudo ./tools/l4t_flash_prerequisites.sh
+
+
+cd $ROOT
+
+
+sudo touch Linux_for_Tegra/source/public/hardware/nvidia/platform/t23x/p3768/kernel-dts/cvb/tegra234-camera-rbpcv4-imx296.dtsi
+sudo touch Linux_for_Tegra/source/public/hardware/nvidia/platform/t23x/p3768/kernel-dts/cvb/tegra234-p3768-camera-rbpcv4-imx296.dtsi
+sudo touch Linux_for_Tegra/source/public/kernel/nvidia/drivers/media/i2c/imx296_mode_tbls.h
+sudo touch Linux_for_Tegra/source/public/kernel/nvidia/drivers/media/i2c/nv_imx296.c
+
+
 sudo ./flashScri.sh $ROOT
 
-exit 0
 
-
-export CROSS_COMPILE_AARCH64_PATH=$1/aarch64--glibc--stable-final
-
-export CROSS_COMPILE_AARCH64=$1/aarch64--glibc--stable-final/bin/aarch64-buildroot-linux-gnu-
-
-#cd /home/engineering/Downloads/VVDN_DON/Linux_for_Tegra/source/public/
-cd $1/Linux_for_Tegra/source/public/
-
-mkdir kernel_out
-./nvbuild.sh -o $PWD/kernel_out
-
-if [ $? -eq 0 ]; then
-echo -e "${RED}***********BUILDING DONE**************${NOCOLOR}"
-else
-echo -e "${GREEN}***********BUILDING FAILED************${NOCOLOR}"
-fi
-
-sudo cp kernel_out/drivers/gpu/nvgpu/nvgpu.ko ../../../Linux_for_Tegra/rootfs/usr/lib/modules/5.10.120-tegra/kernel/drivers/gpu/nvgpu/nvgpu.ko
-
-sudo cp kernel_out/arch/arm64/boot/dts/nvidia/* ../../../Linux_for_Tegra/kernel/dtb
-
-cp kernel_out/arch/arm64/boot/Image ../../../Linux_for_Tegra/kernel/Image
-
-cd ../../../Linux_for_Tegra/rootfs/usr
-
-sudo tar --owner root --group root -cjf kernel_supplements.tbz2 lib/modules
-
-sudo cp ../../../Linux_for_Tegra/rootfs/usr/kernel_supplements.tbz2 ../../../Linux_for_Tegra/kernel/
-
-cd ../../../Linux_for_Tegra/
-
-echo -e "before flashing"
-
-#echo -e "${RED}Kindly put the board in recovery mode else just ignore the
-#warning${NOCOLOR}"
 
 #sudo ./tools/kernel_flash/l4t_initrd_flash.sh --external-device nvme0n1p1 -c
 #tools/kernel_flash/flash_l4t_external.xml -p "-c bootloader/t186ref/cfg/flash_t234_qspi.xml" --showlogs --network usb0
